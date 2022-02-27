1ea04b4a922a652ab7b5e706d1d3658dd654fc49
hannahhan153 <hannah.han@biola.edu>
2022-02-26 20:00:39 -0500

    tests for manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,31 @@
 
-const manager = require('../lib/Manager')
+
+const Employee = require('../lib/Employee')
+const Manager = require('../lib/Manager')
+
+test('creates manager object', () => {
+    const manager = new Manager('Esther');
+
+    expect(manager.name).toBe('Esther');
+})
+
+test('test office number', () => {
+    const test = 100
+    
+    const esther = new Manager('esther', 1, 'manager@gmail.com', test)
+    expect(esther.officenumber).toBe(test)
+})
+
+test('get office number', () => {
+    const test = 100
+
+    const esther = new Manager('esther', 1, 'manager@gmail.com', test)
+    expect(esther.getOfficenumber()).toBe(test)
+})
+
+test('get manager as role', () => {
+    const test = "Manager"
+
+    const esther = new Manager('esther', 1, 'manager@gmail.com', 100, test)
+    expect(esther.getRole()).toBe(test)
+})
