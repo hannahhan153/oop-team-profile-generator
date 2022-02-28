8f2ab91a40aad0d0726685e21c072c6f7b06d0fa
hannahhan153 <hannah.han@biola.edu>
2022-02-28 00:10:44 -0500

    functionality

--- index.js
+++ index.js
@@ -19,97 +19,235 @@ const {
     listenerCount
 } = require('process');
 
-var profilesArray = [];
+var profileArray = [];
 var idArray = [];
 
 // emptyArray.push
-// function initPage() {
+function initPage() {
 
-//     function profileManager() {
+// manager
+function addManager() {
 
-inquirer.prompt([{
-        type: "input",
-        name: "managerName",
-        message: "What is the manager's name? (Required)",
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log("Please enter the team's manager's name.");
-                return false;
+    inquirer.prompt([{
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team's manager's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the manager's ID? (Required)",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the ID of the manager.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: 'What is the email address of the manager? (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the email address of the manager.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerOfficenumber',
+            message: 'What is the office number of the manager? (Required)',
+            validate: officenumberInput => {
+                if (officenumberInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the office number of the manager.');
+                    return false;
+                }
             }
         }
-    },
-    {
-        type: "input",
-        name: "managerId",
-        message: "What is the manager's ID? (Required)",
-        validate: idInput => {
-            if (idInput) {
-                return true;
-            } else {
-                console.log('Please enter the ID of the manager.');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'managerEmail',
-        message: 'What is the email address of the manager? (Required)',
-        validate: emailInput => {
-            if (emailInput) {
-                return true;
-            } else {
-                console.log('Please enter the email address of the manager.');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'managerOfficenumber',
-        message: 'What is the office number of the manager? (Required)',
-        validate: officenumberInput => {
-            if (officenumberInput) {
-                return true;
-            } else {
-                console.log('Please enter the office number of the manager.');
-                return false;
-            }
-        }
-    }
-]).then(input => {
-    const manager = new Manager(input.name, input.id, input.email, input.officenumber);
-    profilesArray.push(manager);
-    idArray.push(input.id)
-    generateProfile();
-});
 
+    ]).then(answers => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficenumber);
+        profileArray.push(manager);
+        idArray.push(answers.managerId)
+        generateProfile();
+
+    });
+}
+// add to team 
 function generateProfile() {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'chooseEmployee',
+        message: 'Who would you like to add next to your team?',
+        choices: ['Engineer', 'Intern', "None"]
+    }]).then(teamChoose => {
+        switch (teamChoose.chooseEmployee) {
+            case 'Engineer':
+                addEngineer();
+                break;
+            case 'Intern':
+                addIntern();
+                break;
+            default:
+            generateHTML();
+        }
+    })
+        
+}
+// engineer
+function addEngineer() {
+    inquirer.prompt([{
+            type: "input",
+            name: "engineerName",
+            message: "What is the engineer's name? (Required)",
+            validate: engineerNameInput => {
+                if (engineerNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the team's engineer's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "engineerId",
+            message: "What is the engineer's ID? (Required)",
+            validate: engineerIdInput => {
+                if (engineerIdInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the ID of the engineer.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: 'What is the email address of the engineer? (Required)',
+            validate: engineerEmailInput => {
+                if (engineerEmailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the email address of the engineer.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: 'What is the github account name of the engineer? (Required)',
+            validate: engineerGithubInput => {
+                if (engineerGithubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the git hub account of the engineer.');
+                    return false;
+                }
+            }
+        }
+    ]).then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub)
+        profileArray.push(engineer);
+        idArray.push(answers.engineerId)
+        generateProfile();
+    });
+}
+
+function addIntern() {
     inquirer.prompt([
         {
-            type: 'list',
-            name: 'chooseEmployee',
-            message: 'Who would you like to add next to your team?',
-            choices: ['Engineer', 'Intern',"None"
-        ]
-    }
-    ]).then(teamChoose => {
-    switch (teamChoose.memberChoose) {
-        case 'Engineer':
-            addEngineer();
-            break;
-        case 'Intern':
-            addIntern();
-            break;
-        default:
-            generateHTML();
-    }
-})
+            type: "input",
+            name: "internName",
+            message: "What is the intern's name? (Required)",
+            validate: internName => {
+                if (internName) {
+                    return true;
+                } else {
+                    console.log("Please enter the team's intern's name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "internId",
+            message: "What is the intern's ID? (Required)",
+            validate: internId => {
+                if (internId) {
+                    return true;
+                } else {
+                    console.log('Please enter the ID of the intern.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: 'What is the email address of the intern? (Required)',
+            validate: internEmail => {
+                if (internEmail) {
+                    return true;
+                } else {
+                    console.log('Please enter the email address of the intern.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: 'What is the name of the school that the intern attended? (Required)',
+            validate: internSchool => {
+                if (internSchool) {
+                    return true;
+                } else {
+                    console.log('Please enter the school name of the intern.');
+                    return false;
+                }
+            }
+        }
+
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool)
+        profileArray.push(intern)
+        idArray.push(answers.internId);
+        generateProfile();
+    });
 }
-//     }
-// }
+
+function generateHTML() {
+    if (!fs.existsSync(path1)) {
+        fs.mkdirSync(path1)
+    }
+    console.log("Team Profile is Loading...");
+    fs.writeFileSync(path2, template(profileArray), "utf-8");
+
+    }
+
+addManager();
+
+}
 
 
-// initPage();
+initPage();
+
--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,7 @@
 const Manager = require("../lib/Manager");
-
+const Employee = require("../lib/Employee")
+const Intern = require('../lib/Intern')
+const Engineer = require('../lib/Engineer')
 module.exports = profile => {
     return `
     <!DOCTYPE html>
@@ -15,11 +17,15 @@ module.exports = profile => {
 
 <body>
     <div class="row">
-        <h1>Team Profile</h1>
+        <h1 class="heading">Team Profile</h1>
+        <h2>${teamProfile(profile)}</h2>
     </div>
 </body>
 </html>
 `
+
+
+   
 };
 
 const teamProfile = profile => {
@@ -31,11 +37,11 @@ const teamProfile = profile => {
                 <img src="https://i.pinimg.com/736x/5b/71/ab/5b71ab4ea082c3c11e77312a64bba835.jpg" alt="Avatar"
                     style="width:100%">
                 <div class="container">
-                    <h4><b>${manager.name}</b></h4>
-                    <h5>${manager.getRole()}</h5>
-                    <p>ID:${manager.id}<p>
-                    <p>Email: <a href="mailto:${manager.email}>${manager.email}</a>
-                    <p>Office Number:${manager.getOfficenumber()}</p>
+                <h4><b>${manager.getName()}</b></h4>
+                <h5>Manager</h5>
+                <p>ID:${manager.getId()}<p>
+                <p class="item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+                <p class="item">Office Number:${manager.getOfficenumber()}</p>
                 </div>
             </div>
         </div>
@@ -49,11 +55,11 @@ const teamProfile = profile => {
             <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/fd44d538650505.598fa11957245.jpg"
                 alt="Avatar" style="width:100%">
             <div class="container">
-                <h4><b>${engineer.name}</b></h4>
-                <h5>${engineer.getRole()}</h5>
+                <h4><b>${engineer.getName()}</b></h4>
+                <h5>Engineer</h5>
                 <p>ID:${engineer.id}</p>
-                <p>Email: <a href="mailto:${engineer.email}>${engineer.email}</a></p>
-                <p>GitHub: <a href="https://github.com/${engineer.getGitHub()}">${engineer.github}</p>
+                <p class="item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></p>
+                <p>GitHub Account: ${engineer.getGithub()}</p>
             </div>
         </div>
     </div>
@@ -67,10 +73,10 @@ const teamProfile = profile => {
                 <img src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/88c1e338650505.598fa119bc679.jpg"
                     alt="Avatar" style="width:100%">
                 <div class="container">
-                    <h4><b>${intern.name}</b></h4>
+                    <h4><b>${intern.getName()}</b></h4>
                     <h5>${intern.getRole()}</h5>
-                    <p>ID:${intern.id}</p>
-                    <p>Email: <a href="mailto:${intern.email}>${intern.email}</a></p>
+                    <p>ID:${intern.getId()}</p>
+                    <p class="item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
                     <p>School:${intern.getSchool()}</p>
                 </div>
             </div>
